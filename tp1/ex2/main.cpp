6b74fbd247b1bb7da6485fc8360d6794e6d41c92
Veos <val.sebb@gmail.com>
2024-01-19 12:02:42 +0100

    tp1 - ex2 - q6

--- tp1/ex2/main.cpp
+++ tp1/ex2/main.cpp
@@ -1,12 +1,50 @@
 #include <iomanip>
 #include <iostream>
+#include <string>
+#include <vector>
 #include "Person.hpp"
 
 int main(int argc, char const *argv[])
 {
-    Person person{"La Faluche", "Palluche"};
+    int nb_person;
 
-    std::cout << "Prenom : " << person.get_surname() << "\nNom : " << person.get_first_name() << std::endl;
+    std::cout << "Nombre de personnes: ";
+
+    std::cin >> nb_person;
+
+    std::vector<Person> person_tab;
+
+    for (int i = 0; i < nb_person; i++)
+    {
+        std::string first_name;
+        std::string surname;
+
+        std::cout << "Prenom: ";
+
+        std::cin >> surname;
+
+        std::cout << "Nom: ";
+
+        std::cin >> first_name;
+
+        person_tab.push_back(Person{first_name, surname});
+    }
+
+    std::cout << "Les personnes sont ";
+
+    for (int i = 0; i < nb_person; i++)
+    {
+        if (i == nb_person - 1)
+        {
+            std::cout << person_tab[i].get_first_name() << " " << person_tab[i].get_surname() << ".";
+        }
+        else
+        {
+            std::cout << person_tab[i].get_first_name() << " " << person_tab[i].get_surname() << ", ";
+        }
+    }
+
+    std::cout << std::endl;
 
     return 0;
 }
