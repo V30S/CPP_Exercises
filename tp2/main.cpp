ee6501bb0cd1c3b4bd212f021bcfebcd5ca7c458
Veos <val.sebb@gmail.com>
2024-01-26 09:40:51 +0100

    tp2 - ex1 - q8

--- tp2/main.cpp
+++ tp2/main.cpp
@@ -7,9 +7,9 @@ int main(int argc, char const *argv[])
 {
     const Card c1{Card_enum::huit, "Pique"};
     c1.print();
-    Card c2{Card_enum::huit, "Carreau"};
+    const Card c2{Card_enum::huit, "Carreau"};
     std::cout << (c2 == c1) << std::endl; // -> 1
-    Card c3{Card_enum::dix, "Carreau"};
+    const Card c3{Card_enum::dix, "Carreau"};
     std::cout << (c2 == c3) << std::endl; // -> 0
     return 0;
 }
