83694f1e8975663d6cd43f16e7d4b528ea888123
Veos <val.sebb@gmail.com>
2024-04-04 11:18:07 +0200

    tp10 - ex1 - A4

---
+++ tp10/src/image.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <array>
+
+template <typename P, size_t W, size_t H>
+class Image
+{
+public:
+    Image() = default;
+
+    Image(const P& pixel)
+    {
+        for (size_t i = 0; i < H; ++i)
+        {
+            for (size_t j = 0; j < W; ++j)
+            {
+                pixels[i][j] = pixel;
+            }
+        }
+    }
+
+    const P& img(size_t i, size_t j) const { return pixels[j][i]; }
+
+    P& img(size_t i, size_t j) { return pixels[j][i]; }
+
+    
+
+private:
+    std::array<std::array<P, W>, H> pixels;
+};
