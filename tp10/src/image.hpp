7a4fd0b25ab6d89fea4f491c290b5fd305078338
Veos <val.sebb@gmail.com>
2024-04-04 11:46:14 +0200

    tp10 - ex1 - A5

--- tp10/src/image.hpp
+++ tp10/src/image.hpp
@@ -23,6 +23,19 @@ public:
 
     P& operator()(size_t i, size_t j) { return pixels[j][i]; }
 
+    const Image& operator+=(const Image& other)
+    {
+        Image result;
+        for (size_t i = 0; i < W; ++i)
+        {
+            for (size_t j = 0; j < H; ++j)
+            {
+                result(i, j) = (*this)(i, j) + other(i, j);
+            }
+        }
+        return result;
+    }
+
 private:
     std::array<std::array<P, W>, H> pixels;
 };
--- tp10/src/midgray.cpp
+++ tp10/src/midgray.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    const auto image = Image<uint8_t, 800, 600>(128u);
+    const auto image = Image<uint8_t, 800, 600>(128);
     for (size_t i = 0; i < 800; ++i)
         for (size_t j = 0; j < 600; ++j)
             assert(image(i, j) == 128u);
